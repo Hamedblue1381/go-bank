e1a84d2b3fda79ba1e7a835d1a2e38410fc853fa
hamedblue1381 <hamed.balanar@gmail.com>
2023-06-22 20:53:31 +0330

    sqlc: generated delete,get,getlist,update

--- bankmodel/account.sql.go
+++ bankmodel/account.sql.go
@@ -3,7 +3,7 @@
 //   sqlc v1.18.0
 // source: account.sql
 
-package hamed
+package bankmodel
 
 import (
 	"context"
@@ -36,4 +36,88 @@ func (q *Queries) CreateAccount(ctx context.Context, arg CreateAccountParams) (A
 		&i.CreatedAt,
 	)
 	return i, err
+}
+
+const deleteAccount = `-- name: DeleteAccount :exec
+DELETE FROM accounts WHERE id = $1
+`
+
+func (q *Queries) DeleteAccount(ctx context.Context, id int64) error {
+	_, err := q.db.ExecContext(ctx, deleteAccount, id)
+	return err
+}
+
+const getAccount = `-- name: GetAccount :one
+SELECT id, owner, balance, curreny, created_at FROM accounts
+WHERE id=$1 LIMIT 1
+`
+
+func (q *Queries) GetAccount(ctx context.Context, id int64) (Account, error) {
+	row := q.db.QueryRowContext(ctx, getAccount, id)
+	var i Account
+	err := row.Scan(
+		&i.ID,
+		&i.Owner,
+		&i.Balance,
+		&i.Curreny,
+		&i.CreatedAt,
+	)
+	return i, err
+}
+
+const listAccount = `-- name: ListAccount :many
+SELECT id, owner, balance, curreny, created_at FROM accounts
+ORDER BY id
+LIMIT $1
+OFFSET $2
+`
+
+type ListAccountParams struct {
+	Limit  int32
+	Offset int32
+}
+
+func (q *Queries) ListAccount(ctx context.Context, arg ListAccountParams) ([]Account, error) {
+	rows, err := q.db.QueryContext(ctx, listAccount, arg.Limit, arg.Offset)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	var items []Account
+	for rows.Next() {
+		var i Account
+		if err := rows.Scan(
+			&i.ID,
+			&i.Owner,
+			&i.Balance,
+			&i.Curreny,
+			&i.CreatedAt,
+		); err != nil {
+			return nil, err
+		}
+		items = append(items, i)
+	}
+	if err := rows.Close(); err != nil {
+		return nil, err
+	}
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+	return items, nil
+}
+
+const updateAccount = `-- name: UpdateAccount :exec
+UPDATE accounts
+SET balance = $2
+WHERE id = $1
+`
+
+type UpdateAccountParams struct {
+	ID      int64
+	Balance int64
+}
+
+func (q *Queries) UpdateAccount(ctx context.Context, arg UpdateAccountParams) error {
+	_, err := q.db.ExecContext(ctx, updateAccount, arg.ID, arg.Balance)
+	return err
 }
