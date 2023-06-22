751f1cf892c63ee806992e5e97edeb58d4d0f2a2
Hamed Malek Mohammadi <hamed.balanar@gmail.com>
2023-06-22 20:26:12 +0330

    feat: add get, getList, update, delete queries

--- db/query/account.sql
+++ db/query/account.sql
@@ -6,3 +6,22 @@ INSERT INTO accounts (
 ) VALUES (
     $1, $2, $3
 ) RETURNING *;
+
+
+-- name: GetAccount :one
+SELECT * FROM accounts
+WHERE id=$1 LIMIT 1;
+
+-- name: ListAccount :many
+SELECT * FROM accounts
+ORDER BY id
+LIMIT $1
+OFFSET $2 ;
+
+-- name: UpdateAccount :exec
+UPDATE accounts
+SET balance = $2
+WHERE id = $1;
+
+-- name: DeleteAccount :exec
+DELETE FROM accounts WHERE id = $1;
