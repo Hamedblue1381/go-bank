c1946a26cb74908bcb635eaa74b5292d285fcb36
hamedblue1381 <hamed.balanar@gmail.com>
2023-06-23 14:46:26 +0330

    feat: add db con test

---
+++ bankmodel/main_test.go
@@ -0,0 +1,29 @@
+package db
+
+import (
+	"database/sql"
+	"log"
+	"os"
+	"testing"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	dbDriver = "postgres"
+	dbSource = "postgresql://root:secret@localhost:5432/hamed_bank?sslmode=disable"
+)
+
+var testQueries *Queries
+
+func TestMain(m *testing.M) {
+	conn, err := sql.Open(dbDriver, dbSource)
+	if err != nil {
+		log.Fatal("cannot connect to db:", err)
+
+	}
+
+	testQueries = New(conn)
+
+	os.Exit(m.Run())
+}
