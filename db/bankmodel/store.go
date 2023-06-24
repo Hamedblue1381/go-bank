51beb0017366b58639b1c316255b9964000dc6a4
hamedblue1381 <hamed.balanar@gmail.com>
2023-06-24 14:42:20 +0330

    feat: add update balance preventing deadlock

--- db/bankmodel/store.go
+++ db/bankmodel/store.go
@@ -77,8 +77,22 @@ func (store *Store) TransferTx(ctx context.Context, arg TransferTxParams) (Trans
 			return err
 		}
 
-		//TODO : update accounts balance
+		//update accounts balance
+		result.FromAccount, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
+			ID:     arg.FromAccountID,
+			Amount: -arg.Amount,
+		})
+		if err != nil {
+			return err
+		}
 
+		result.ToAccount, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
+			ID:     arg.ToAccountID,
+			Amount: arg.Amount,
+		})
+		if err != nil {
+			return err
+		}
 		return nil
 	})
 	return result, err
