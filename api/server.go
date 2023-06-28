13fc7226e3fa7a28c91e9def1c9ea58b52c2b0e2
hamedblue1381 <hamed.balanar@gmail.com>
2023-06-28 20:18:25 +0330

    fix typo

--- api/server.go
+++ api/server.go
@@ -16,7 +16,7 @@ func NewServer(store *db.Store) *Server {
 
 	router.POST("/accounts", server.createAccount)
 	router.GET("/accounts/:id", server.getAccount)
-	router.GET("/accounts/", server.listAccount)
+	router.GET("/accounts", server.listAccount)
 	router.DELETE("/accounts/:id", server.deleteAccount)
 	router.PUT("/accounts/:id", server.updateAccount)
 
