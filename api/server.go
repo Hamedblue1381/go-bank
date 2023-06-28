76985e295003f6f0fbf17347bead480ff756f0b3
hamedblue1381 <hamed.balanar@gmail.com>
2023-06-28 18:15:56 +0330

    feat: add router api

--- api/server.go
+++ api/server.go
@@ -14,8 +14,20 @@ func NewServer(store *db.Store) *Server {
 	server := &Server{store: store}
 	router := gin.Default()
 
-	router.POST("/accounts")
+	router.POST("/accounts", server.createAccount)
+	router.GET("/accounts/:id", server.getAccount)
+	router.GET("/accounts/", server.listAccount)
+	router.DELETE("/accounts/:id", server.deleteAccount)
+	router.PUT("/accounts/:id", server.updateAccount)
 
 	server.router = router
 	return server
+}
+
+func (server *Server) Start(address string) error {
+	return server.router.Run(address)
+}
+
+func errorResponse(err error) gin.H {
+	return gin.H{"error": err.Error()}
 }
