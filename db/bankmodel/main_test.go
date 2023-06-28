48a11b145c07856b1e924469f330aa56772a2df5
hamedblue1381 <hamed.balanar@gmail.com>
2023-06-28 20:18:51 +0330

    feat: remove hard code & add viper config load

--- db/bankmodel/main_test.go
+++ db/bankmodel/main_test.go
@@ -6,12 +6,8 @@ import (
 	"os"
 	"testing"
 
+	"github.com/HamedBlue1381/go-postgres-gRPC/util"
 	_ "github.com/lib/pq"
-)
-
-const (
-	dbDriver = "postgres"
-	dbSource = "postgresql://root:secret@localhost:5432/hamed_bank?sslmode=disable"
 )
 
 var testQueries *Queries
@@ -19,7 +15,11 @@ var testDB *sql.DB
 
 func TestMain(m *testing.M) {
 	var err error
-	testDB, err = sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig("../..")
+	if err != nil {
+		log.Fatal(err)
+	}
+	testDB, err = sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 
