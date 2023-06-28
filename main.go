b11f5cb53b5f82a4f05b53cf315059afd137dc58
hamedblue1381 <hamed.balanar@gmail.com>
2023-06-28 20:18:09 +0330

    feat: remove hardcoded and load viper config

--- main.go
+++ main.go
@@ -6,24 +6,23 @@ import (
 
 	"github.com/HamedBlue1381/go-postgres-gRPC/api"
 	db "github.com/HamedBlue1381/go-postgres-gRPC/db/bankmodel"
+	"github.com/HamedBlue1381/go-postgres-gRPC/util"
 	_ "github.com/lib/pq"
 )
 
-const (
-	dbDriver      = "postgres"
-	dbSource      = "postgresql://root:secret@localhost:5432/hamed_bank?sslmode=disable"
-	serverAddress = "0.0.0.0:8000"
-)
-
 func main() {
-	conn, err := sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
 
-	err = server.Start(serverAddress)
+	err = server.Start(config.ServerAddress)
 	if err != nil {
 		log.Fatal("cannot start the server", err)
 	}
