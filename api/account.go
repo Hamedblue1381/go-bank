ad9019fe9516110dd355a74b285f4eba51d7445d
hamedblue1381 <hamed.balanar@gmail.com>
2023-06-28 20:18:16 +0330

    fix typo

--- api/account.go
+++ api/account.go
@@ -104,7 +104,7 @@ func (server *Server) updateAccount(ctx *gin.Context) {
 
 type listAccountRequest struct {
 	PageID   int32 `form:"page_id" binding:"required,min=1"`
-	PageSize int32 `form:"page_size" binding:"required,min=5,max10"`
+	PageSize int32 `form:"page_size" binding:"required,min=5,max=10"`
 }
 
 func (server *Server) listAccount(ctx *gin.Context) {
