d1a9552303f234c751a60dd5f6c0dc9344ca3132
hamedblue1381 <hamed.balanar@gmail.com>
2023-06-30 16:13:22 +0330

    feat: add error handler constraint key violation

--- api/account.go
+++ api/account.go
@@ -6,6 +6,7 @@ import (
 
 	db "github.com/HamedBlue1381/go-postgres-gRPC/db/bankmodel"
 	"github.com/gin-gonic/gin"
+	"github.com/lib/pq"
 )
 
 type createAccountRequest struct {
@@ -27,6 +28,14 @@ func (server *Server) createAccount(ctx *gin.Context) {
 	}
 	account, err := server.store.CreateAccount(ctx, arg)
 	if err != nil {
+		if pqErr, ok := err.(*pq.Error); ok {
+			switch pqErr.Code.Name() {
+			case "foreign_key_violation", "unique_key_violation":
+				ctx.JSON(http.StatusForbidden, errorResponse(err))
+				return
+			}
+
+		}
 		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
 		return
 	}
