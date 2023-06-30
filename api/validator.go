a84fe425f05fbcd473a09f9801a47e9fa0f3a09a
hamedblue1381 <hamed.balanar@gmail.com>
2023-06-30 10:48:23 +0330

    feat: add Supported currency validator

---
+++ api/validator.go
@@ -0,0 +1,13 @@
+package api
+
+import (
+	"github.com/HamedBlue1381/go-postgres-gRPC/util"
+	"github.com/go-playground/validator/v10"
+)
+
+var validCurrency validator.Func = func(fieldlevel validator.FieldLevel) bool {
+	if currency, ok := fieldlevel.Field().Interface().(string); ok {
+		return util.IsSuppotortedCurrnecy(currency)
+	}
+	return false
+}
