d6a5052f54ef88e9a1dba9e0425b115a357b9930
hamedblue1381 <hamed.balanar@gmail.com>
2023-06-30 16:14:40 +0330

    feat: add random email generator

--- util/random.go
+++ util/random.go
@@ -1,6 +1,7 @@
 package util
 
 import (
+	"fmt"
 	"math/rand"
 	"strings"
 	"time"
@@ -40,4 +41,7 @@ func RandomCurrency() string {
 	currencies := []string{EUR, USD, CAD, RIAL}
 	n := len(currencies)
 	return currencies[rand.Intn(n)]
+}
+func RandomEmail() string {
+	return fmt.Sprintf("%s@email.com", RandomString(6))
 }
