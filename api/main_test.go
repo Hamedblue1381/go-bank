1ae682c9ec2cd264dd13847109fce38f3aa983ae
hamedblue1381 <hamed.balanar@gmail.com>
2023-07-06 16:37:22 +0330

    feat: add newTestServer func with test token key

--- api/main_test.go
+++ api/main_test.go
@@ -3,9 +3,23 @@ package api
 import (
 	"os"
 	"testing"
+	"time"
 
+	db "github.com/HamedBlue1381/go-postgres-gRPC/db/bankmodel"
+	"github.com/HamedBlue1381/go-postgres-gRPC/util"
 	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/require"
 )
+
+func newTestServer(t *testing.T, store db.Store) *Server {
+	config := util.Config{
+		TokenSymmetricKey:   util.RandomString(32),
+		AccessTokenDuration: time.Minute,
+	}
+	server, err := NewServer(config, store)
+	require.NoError(t, err)
+	return server
+}
 
 func TestMain(m *testing.M) {
 	gin.SetMode(gin.TestMode)
