fc6aa0d531de240cf41b3ac96ee965f0a8f584ad
hamedblue1381 <hamed.balanar@gmail.com>
2023-07-06 12:21:26 +0330

    feat: add maker interface

---
+++ token/maker.go
@@ -0,0 +1,11 @@
+package token
+
+import (
+	"time"
+)
+
+type Maker interface {
+	CreateToken(username string, duration time.Duration) (string, *Payload, error)
+
+	VerifyToken(token string) (*Payload, error)
+}
