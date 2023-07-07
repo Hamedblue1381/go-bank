8265909019d26b253c210c3d805398b896a459ae
hamedblue1381 <hamed.balanar@gmail.com>
2023-07-07 20:32:39 +0330

    feat: add auth middleware

--- api/server.go
+++ api/server.go
@@ -42,13 +42,15 @@ func (server *Server) setUpRouter() {
 	router.POST("/users", server.createUser)
 	router.POST("/users/login", server.loginUser)
 
-	router.POST("/accounts", server.createAccount)
-	router.GET("/accounts/:id", server.getAccount)
-	router.GET("/accounts", server.listAccount)
-	router.DELETE("/accounts/:id", server.deleteAccount)
-	router.PUT("/accounts/:id", server.updateAccount)
+	authRoutes := router.Group("/").Use(authMiddleware(server.tokenMaker))
 
-	router.POST("/transfers", server.createTransfer)
+	authRoutes.POST("/accounts", server.createAccount)
+	authRoutes.GET("/accounts/:id", server.getAccount)
+	authRoutes.GET("/accounts", server.listAccount)
+	authRoutes.DELETE("/accounts/:id", server.deleteAccount)
+	authRoutes.PUT("/accounts/:id", server.updateAccount)
+
+	authRoutes.POST("/transfers", server.createTransfer)
 
 	server.router = router
 
