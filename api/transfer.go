80c416c34f5d2534847632da3f4173a4b1c28493
hamedblue1381 <hamed.balanar@gmail.com>
2023-07-08 17:14:13 +0330

    fix : typo

--- api/transfer.go
+++ api/transfer.go
@@ -12,8 +12,8 @@ import (
 )
 
 type transferRequest struct {
-	FromAccountID int64  `json:"from_account_id" binding:"required, min=1"`
-	ToAccountID   int64  `json:"to_account_id" binding:"required, min=1"`
+	FromAccountID int64  `json:"from_account_id" binding:"required,min=1"`
+	ToAccountID   int64  `json:"to_account_id" binding:"required,min=1"`
 	Amount        int64  `json:"amount" binding:"required,gt=0"`
 	Currency      string `json:"currency" binding:"required,currency"`
 }
