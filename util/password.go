f86d5ac2b56a797d22ba5b796794149cdad8cdec
hamedblue1381 <hamed.balanar@gmail.com>
2023-07-08 17:16:46 +0330

    fix: wrong order in checkpassword

--- util/password.go
+++ util/password.go
@@ -6,6 +6,7 @@ import (
 	"golang.org/x/crypto/bcrypt"
 )
 
+// HashPassword returns the bcrypt hash of the password
 func HashPassword(password string) (string, error) {
 	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 	if err != nil {
@@ -14,6 +15,7 @@ func HashPassword(password string) (string, error) {
 	return string(hashedPassword), nil
 }
 
+// CheckPassword checks if the provided password is correct or not
 func CheckPassword(password string, hashedPassword string) error {
-	return bcrypt.CompareHashAndPassword([]byte(password), []byte(hashedPassword))
+	return bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
 }
