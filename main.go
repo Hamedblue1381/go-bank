e0f66d02a36f923457d73e93179bba919050de11
hamedblue1381 <hamed.balanar@gmail.com>
2023-07-21 23:23:47 +0330

    feat: add grpc server

--- main.go
+++ main.go
@@ -3,11 +3,16 @@ package main
 import (
 	"database/sql"
 	"log"
+	"net"
 
 	"github.com/HamedBlue1381/hamed-bank/api"
 	db "github.com/HamedBlue1381/hamed-bank/db/bankmodel"
+	"github.com/HamedBlue1381/hamed-bank/gapi"
+	"github.com/HamedBlue1381/hamed-bank/pb"
 	"github.com/HamedBlue1381/hamed-bank/util"
 	_ "github.com/lib/pq"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 )
 
 func main() {
@@ -20,12 +25,36 @@ func main() {
 		log.Fatal("cannot connect to db:", err)
 	}
 	store := db.NewStore(conn)
+	runGrpcServer(config, store)
+}
+
+func runGrpcServer(config util.Config, store db.Store) {
+	server, err := gapi.NewServer(config, store)
+	if err != nil {
+		log.Fatal("cannot create the server", err)
+	}
+	grpcServer := grpc.NewServer()
+	pb.RegisterHamedBankServer(grpcServer, server)
+	reflection.Register(grpcServer)
+
+	listener, err := net.Listen("tcp", config.GRPCServerAddress)
+	if err != nil {
+		log.Fatal("cannot create the listener", err)
+	}
+
+	log.Printf("Starting the gRPC server: %s", listener.Addr().String())
+	err = grpcServer.Serve(listener)
+	if err != nil {
+		log.Fatal("cannot start the gRPC server", err)
+	}
+}
+func runGinServer(config util.Config, store db.Store) {
 	server, err := api.NewServer(config, store)
 	if err != nil {
 		log.Fatal("cannot create the server", err)
 	}
 
-	err = server.Start(config.ServerAddress)
+	err = server.Start(config.HTTPServerAddress)
 	if err != nil {
 		log.Fatal("cannot start the server", err)
 	}
