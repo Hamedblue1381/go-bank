0a0c38a0c106040f6605d6455fc4d5ed0e6806ef
hamedblue1381 <hamed.balanar@gmail.com>
2023-07-26 16:21:53 +0330

    feat: add http gateway server

--- main.go
+++ main.go
@@ -1,18 +1,22 @@
 package main
 
 import (
+	"context"
 	"database/sql"
 	"log"
 	"net"
+	"net/http"
 
 	"github.com/HamedBlue1381/hamed-bank/api"
 	db "github.com/HamedBlue1381/hamed-bank/db/bankmodel"
 	"github.com/HamedBlue1381/hamed-bank/gapi"
 	"github.com/HamedBlue1381/hamed-bank/pb"
 	"github.com/HamedBlue1381/hamed-bank/util"
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	_ "github.com/lib/pq"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
+	"google.golang.org/protobuf/encoding/protojson"
 )
 
 func main() {
@@ -25,6 +29,7 @@ func main() {
 		log.Fatal("cannot connect to db:", err)
 	}
 	store := db.NewStore(conn)
+	go runGatewayServer(config, store)
 	runGrpcServer(config, store)
 }
 
@@ -48,6 +53,47 @@ func runGrpcServer(config util.Config, store db.Store) {
 		log.Fatal("cannot start the gRPC server", err)
 	}
 }
+
+func runGatewayServer(config util.Config, store db.Store) {
+	server, err := gapi.NewServer(config, store)
+	if err != nil {
+		log.Fatal("cannot create the server", err)
+	}
+	jsonOptions :=
+		runtime.WithMarshalerOption(runtime.MIMEWildcard, &runtime.JSONPb{
+			MarshalOptions: protojson.MarshalOptions{
+				UseProtoNames: true,
+			},
+			UnmarshalOptions: protojson.UnmarshalOptions{
+				DiscardUnknown: true,
+			},
+		})
+
+	grpcMux := runtime.NewServeMux(jsonOptions)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	err = pb.RegisterHamedBankHandlerServer(ctx, grpcMux, server)
+	if err != nil {
+		log.Fatal("cannot register handler server:", err)
+	}
+
+	mux := http.NewServeMux()
+	mux.Handle("/", grpcMux)
+
+	listener, err := net.Listen("tcp", config.HTTPServerAddress)
+	if err != nil {
+		log.Fatal("cannot create the listener", err)
+	}
+
+	log.Printf("Starting the HTTP Gateway server: %s", listener.Addr().String())
+	err = http.Serve(listener, mux)
+	if err != nil {
+		log.Fatal("cannot start the HTTP gateway server", err)
+	}
+}
+
 func runGinServer(config util.Config, store db.Store) {
 	server, err := api.NewServer(config, store)
 	if err != nil {
