41d03cd2ff593a3df1849a1b83702da9330e69e1
hamedblue1381 <hamed.balanar@gmail.com>
2023-07-31 13:26:23 +0330

    feat: add user update rpc handler + authorization

--- db/bankmodel/user.sql.go
+++ db/bankmodel/user.sql.go
@@ -70,23 +70,26 @@ const updateUser = `-- name: UpdateUser :one
 UPDATE users
 SET
  hashed_password= COALESCE($1, hashed_password ),
- full_name=COALESCE($2, full_name ),
- email=COALESCE($3, email )
+ password_changed_at= COALESCE($2, password_changed_at ),
+ full_name=COALESCE($3, full_name ),
+ email=COALESCE($4, email )
 WHERE
- username = $4
+ username = $5
 RETURNING username, hashed_password, full_name, email, password_changed_at, created_at
 `
 
 type UpdateUserParams struct {
-	HashedPassword sql.NullString
-	FullName       sql.NullString
-	Email          sql.NullString
-	Username       string
+	HashedPassword    sql.NullString
+	PasswordChangedAt sql.NullTime
+	FullName          sql.NullString
+	Email             sql.NullString
+	Username          string
 }
 
 func (q *Queries) UpdateUser(ctx context.Context, arg UpdateUserParams) (User, error) {
 	row := q.db.QueryRowContext(ctx, updateUser,
 		arg.HashedPassword,
+		arg.PasswordChangedAt,
 		arg.FullName,
 		arg.Email,
 		arg.Username,
---
+++ gapi/rpc_update_user.go
@@ -0,0 +1,95 @@
+package gapi
+
+import (
+	"context"
+	"database/sql"
+	"time"
+
+	db "github.com/HamedBlue1381/hamed-bank/db/bankmodel"
+	"github.com/HamedBlue1381/hamed-bank/pb"
+	"github.com/HamedBlue1381/hamed-bank/util"
+	"github.com/HamedBlue1381/hamed-bank/val"
+	"google.golang.org/genproto/googleapis/rpc/errdetails"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+func (server *Server) UpdateUser(ctx context.Context, req *pb.UpdateUserRequest) (*pb.UpdateUserResponse, error) {
+	authPayload, err := server.authorizeUser(ctx)
+	if err != nil {
+		return nil, unauthenticatedError(err)
+	}
+	violations := validateUpdateUserRequest(req)
+	if violations != nil {
+		return nil, invalidArgumentError(violations)
+	}
+
+	if authPayload.Username != req.GetUsername() {
+		return nil, status.Errorf(codes.PermissionDenied, "permission denied!")
+	}
+
+	arg := db.UpdateUserParams{
+		Username: req.GetUsername(),
+		FullName: sql.NullString{
+			String: req.GetFullName(),
+			Valid:  req.FullName != nil,
+		},
+		Email: sql.NullString{
+			String: req.GetEmail(),
+			Valid:  req.Email != nil,
+		},
+	}
+
+	if req.Password != nil {
+		hashedPassword, err := util.HashPassword(req.GetPassword())
+		if err != nil {
+			return nil, status.Errorf(codes.Internal, "failed to hash password : %s", err)
+		}
+		arg.HashedPassword = sql.NullString{
+			String: hashedPassword,
+			Valid:  true,
+		}
+		arg.PasswordChangedAt = sql.NullTime{
+			Time:  time.Now(),
+			Valid: true,
+		}
+	}
+
+	user, err := server.store.UpdateUser(ctx, arg)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return nil, status.Errorf(codes.NotFound, "user not found")
+		}
+		return nil, status.Errorf(codes.Internal, "failed to Update user")
+
+	}
+	rsp := &pb.UpdateUserResponse{
+		User: convertUser(user),
+	}
+	return rsp, nil
+}
+func validateUpdateUserRequest(req *pb.UpdateUserRequest) (violations []*errdetails.BadRequest_FieldViolation) {
+	if err := val.ValidateUsername(req.GetUsername()); err != nil {
+		violations = append(violations, fieldViolation("username", err))
+	}
+
+	if req.Password != nil {
+		if err := val.ValidatePassword(req.GetPassword()); err != nil {
+			violations = append(violations, fieldViolation("password", err))
+		}
+	}
+
+	if req.FullName != nil {
+		if err := val.ValidateFullName(req.GetFullName()); err != nil {
+			violations = append(violations, fieldViolation("full_name", err))
+		}
+	}
+
+	if req.Email != nil {
+		if err := val.ValidateEmail(req.GetEmail()); err != nil {
+			violations = append(violations, fieldViolation("email", err))
+		}
+	}
+
+	return violations
+}
