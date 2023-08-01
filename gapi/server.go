4babbcf536cbd9d0bf6f3dda0346b60d966d9742
hamedblue1381 <hamed.balanar@gmail.com>
2023-08-01 17:45:00 +0330

    feat: add task distributor to server struct

--- gapi/server.go
+++ gapi/server.go
@@ -7,24 +7,27 @@ import (
 	"github.com/HamedBlue1381/hamed-bank/pb"
 	"github.com/HamedBlue1381/hamed-bank/token"
 	"github.com/HamedBlue1381/hamed-bank/util"
+	"github.com/HamedBlue1381/hamed-bank/worker"
 )
 
 type Server struct {
 	pb.UnimplementedHamedBankServer
-	config     util.Config
-	store      db.Store
-	tokenMaker token.Maker
+	config          util.Config
+	store           db.Store
+	tokenMaker      token.Maker
+	taskDistributor worker.TaskDistributor
 }
 
-func NewServer(config util.Config, store db.Store) (*Server, error) {
+func NewServer(config util.Config, store db.Store, taskDistributor worker.TaskDistributor) (*Server, error) {
 	tokenMaker, err := token.NewPasetoMaker(config.TokenSymmetricKey)
 	if err != nil {
 		return nil, fmt.Errorf("cant create token maker: %w", err)
 	}
 	server := &Server{
-		config:     config,
-		store:      store,
-		tokenMaker: tokenMaker,
+		config:          config,
+		store:           store,
+		tokenMaker:      tokenMaker,
+		taskDistributor: taskDistributor,
 	}
 
 	return server, nil
