8701f1b865a73bfccd64b99ba2bff7f94ed023f3
hamedblue1381 <hamed.balanar@gmail.com>
2023-08-05 16:50:45 +0330

    feat: add verify email on db and email sender

--- worker/task_send_verify_email.go
+++ worker/task_send_verify_email.go
@@ -5,6 +5,8 @@ import (
 	"encoding/json"
 	"fmt"
 
+	db "github.com/HamedBlue1381/hamed-bank/db/bankmodel"
+	"github.com/HamedBlue1381/hamed-bank/util"
 	"github.com/hibiken/asynq"
 	"github.com/rs/zerolog/log"
 )
@@ -45,7 +47,29 @@ func (processor *RediTaskProcessor) ProcessTaskSendVerifyEmail(ctx context.Conte
 		// }
 		return fmt.Errorf("failed to get user: %w", err)
 	}
-	//TODO: send email to user
+
+	verifyEmail, err := processor.store.CreateVerifyEmail(ctx, db.CreateVerifyEmailParams{
+		Username:   user.Username,
+		Email:      user.Email,
+		SecretCode: util.RandomString(32),
+	})
+	if err != nil {
+		return fmt.Errorf("failed to create verify email: %w", err)
+	}
+
+	subject := "Welcome to Hamed Bank"
+	verifyUrl := fmt.Sprintf("https://localhost:8000/verify_email?id=%d&secret_code=%s",
+		verifyEmail.ID, verifyEmail.SecretCode)
+	content := fmt.Sprintf(`Hello %s, <br/>
+	Thank you for registering in our bank
+	please <a href"%s">click here </a> to verify your email address. <br/>`,
+		user.FullName, verifyUrl)
+	to := []string{user.Email}
+
+	err = processor.mailer.SendEmail(subject, content, to, nil, nil, nil)
+	if err != nil {
+		return fmt.Errorf("failed to send verify email: %w", err)
+	}
 	log.Info().Str("type", task.Type()).Bytes("payload", task.Payload()).
 		Str("email", user.Email).Msg("processed task")
 
